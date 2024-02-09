3a7efa26d2aef602410cec16ffdef408085ccf39
XVI-IX <oladoja14@gmail.com>
2024-02-09 16:04:41 +0100

    update wishlist schema

--- src/wishlist/wishlist.module.ts
+++ src/wishlist/wishlist.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WishlistService } from './wishlist.service';
 import { WishlistController } from './wishlist.controller';
+import { PostgresModule } from 'src/postgres/postgres.module';
 
 @Module({
   controllers: [WishlistController],
   providers: [WishlistService],
+  imports: [PostgresModule],
 })
 export class WishlistModule {}
--- src/wishlist/wishlist.service.ts
+++ src/wishlist/wishlist.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { PostgresService } from 'src/postgres/postgres.service';
 
 @Injectable()
-export class WishlistService {}
+export class WishlistService {
+  constructor (
+    private pg: PostgresService
+  ) {}
+
+  async addToWishlist(user: any, product_id: string) {
+    try {
+      const query = 
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async getWishlist(user: any) {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+  async deleteFromWishlist(user: any, product_id: string) {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async clearWishlist(user: any) {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
